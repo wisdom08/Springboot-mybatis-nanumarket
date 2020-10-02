6d5d71cacc2664ae612e450f4c4ec57e4bd040bb
wisdom08 <ji4168aw@gmail.com>
2020-10-02 20:30:00 +0900

    Initial commit

---
+++ src/main/java/com/wisdom08/NanumarketApplication.java
@@ -0,0 +1,13 @@
+package com.wisdom08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanumarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NanumarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wisdom08/NanumarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisdom08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanumarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
