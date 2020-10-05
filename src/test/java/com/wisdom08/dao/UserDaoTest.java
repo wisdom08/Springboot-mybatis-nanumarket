855ffd73ad624871b8624f6a8702bee6710dc267
wisdom08 <ji4168aw@gmail.com>
2020-10-05 23:27:49 +0900

    [TEST] test_회원가입() #3

--- src/test/java/com/wisdom08/dao/UserDaoTest.java
+++ src/test/java/com/wisdom08/dao/UserDaoTest.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,4 +32,19 @@ class UserDaoTest {
     }
 
 
+    @Test
+    @Transactional // insert 된 거 무시한다. rollback
+    void test_회원가입() {
+        User user;
+        user = userDao.login("123a", "1111");
+        System.out.println("회원가입 전" + user);
+        assertNull(user);
+        
+        user = new User("123a", "123a@a.com", "1111");
+        userDao.join(user); //test 끝나면 insert 된 거 취소시킨다.  //시퀀스 할당 후 rollback 되어서 버려진다.
+
+        user= userDao.login(user.getId(), user.getPassword());
+        System.out.println("회원가입 후"+user);
+        assertNotNull(user);
+    }
 }
